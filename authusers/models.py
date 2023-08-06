82d118f440248870af955c491a72c555e83ae17f
pedrosavio <pedro_savio777@hotmail.com>
2023-08-06 17:34:46 -0300

    add role

--- authusers/models.py
+++ authusers/models.py
@@ -3,9 +3,16 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    ROLE_CHOICES = [
+        ('adm', 'Administrador'),
+        ('gm', 'GM'),
+        ('tec', 'Técnico'),
+    ]
+
     matricula = models.CharField(max_length=4, unique=True)
     name = models.CharField(max_length=60)
     senha = models.CharField(max_length=4)
+    role = models.CharField(max_length=3, choices=ROLE_CHOICES, default='tec')
 
     def __str__(self):
         return self.matricula
--- authusers/views.py
+++ authusers/views.py
@@ -25,17 +25,20 @@ def cadastro(request):
         matricula = data.get('matricula')
         name = data.get('name')
         senha = data.get('senha')
+        role = data.get('role', 'tec')  # Define 'tec' como valor padrão caso 'role' não esteja presente
+
         if len(matricula) == 4 and len(senha) == 4:
-            user, created = User.objects.get_or_create(matricula=matricula, senha=senha, name=name)
+            user, created = User.objects.get_or_create(matricula=matricula, senha=senha, name=name, role=role)
             if created:
-                return JsonResponse({'message': 'Cadastro realizado com sucesso!', 'matricula': user.matricula, 'name': user.name})
+                return JsonResponse({'message': 'Cadastro realizado com sucesso!', 'matricula': user.matricula, 'name': user.name, 'role': user.role})
             else:
                 return JsonResponse({'message': 'Usuário já cadastrado!'}, status=400)
         else:
             return JsonResponse({'message': 'Matrícula e senha devem ter 4 dígitos!'}, status=400)
     return JsonResponse({'message': 'Método inválido! Use POST para cadastro.'}, status=400)
 
+
 def listar_usuarios(request):
     users = User.objects.all()
-    data = [{'matricula': user.matricula, 'name': user.name} for user in users]
+    data = [{'matricula': user.matricula, 'name': user.name, "role": user.role} for user in users]
     return JsonResponse(data, safe=False)
